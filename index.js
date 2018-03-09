6217ab4632cad6f0c3e54fc074895dfe19d1793a
Sunitha Ramakrishnan <ramakrishnansunitha950@gmail.com>
2018-03-08 18:22:40 -0800

    did all my debugging to get the seaches render page correct

--- index.js
+++ index.js
@@ -1,134 +1,117 @@
-// Get references to the tbody element, input field and button
-var $tbody = document.querySelector('tbody');
- 
+// Getting references
+var $tbody = document.querySelector("#table-body");
+var $dateInput = document.querySelector("#date-input");
+var $cityInput = document.querySelector("#city-input");
+var $stateInput = document.querySelector("#state-input");
+var $countryInput = document.querySelector("#country-input");
+var $shapeInput = document.querySelector("#shape-input");
+var $submitButton = document.querySelector("#submit");
 
-var $searchBtn = document.querySelector('#search_button');
-
-// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-$searchBtn.addEventListener('click', handleSearchButtonClick);
-//document.getElementById("myForm").addEventListener("submit", handleSearchButtonClick, true);
-
-
-// 
-// Level 1: Automatic Table and Date Search
-// Create a basic HTML web page.
-
-// Using the ufo dataset provided in the form of a JavaScript object, 
-//write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-
-// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
-// Add an input tag to your HTML document and write JavaScript code that will search through the 
-// date/time column to find rows that match user input.
-
-
-// Set filteredSightings to dataSet initially
-// dataSet comes from the data.js file
+// Filtered list
 var filteredSightings = dataSet;
 
-// renderTable renders the filtereSightings to the tbody
+// Set starting index and results per page
+var startingIndex = 0;
+var resultsPerPage = 1000;
+
+// Rendering table
+renderTable();
+
+// Function to render table
 function renderTable() {
 
+    // Set the value of ending index
+    var endingIndex = startingIndex + resultsPerPage;
 
-  // First clear the table body, Then iterate over all the records 
-  // in filteredSightings and retrieve all the fields within each record to display as a 
-  // table (by adding rows to the table body)
-  $tbody.innerHTML = '';
-
-  for (var i = 0; i < filteredSightings.length; i++) {
-    // Get get the current record object and its fields
-    var record = filteredSightings[i];
-    var fields = Object.keys(record);
-    // Create a new row in the tbody, set the index to be i + startingIndex
-    var $row = $tbody.insertRow(i);
-    for (var j = 0; j < fields.length; j++) {
-      // For every field in the record object, create a new cell at set its inner text to be the 
-      //current value at the current record's field
-      var field = fields[j];
-      var $cell = $row.insertCell(j);
-      $cell.innerText = record[field];
-    }
-  }
-}
-
-// The function hangleSearchButtonClick is the building a new table
-// of filteredSightings array by filtering the entire dataSet
-// using a callback filter.
-
-function handleSearchButtonClick() {
-  // Retrieve the value of various fields in the search form for processing
-  // by the filter callback function
-  event.preventDefault();
-
-  var $dateInput = document.querySelector('#date');
-  var $stateInput = document.querySelector('#state');
-  var $countryInput = document.querySelector('#country');
-  var $cityInput = document.querySelector('#city');
-  var $shapeInput = document.querySelector('#shape');
-
-
-  // Set filteredSightings to an array of all signting whose "state" matches the filter
-  filteredSightings = dataSet.filter(function(sighting) {
-
-    //You can just check if the variable has a truthy value or not. That means if(filterState) {}
-    // if( value ) {}
-    // will evaluate to true if value is not: 
-    dateFound = cityFound = stateFound = countryFound = shapeFound = false;
-    if ($stateInput) {
-       var recordState = sighting.state.trim().toLowerCase();
-       var filterState = $stateInput.value.trim().toLowerCase(); 
-       if (recordState === filterState ) {
-          stateFound = true;
-       }
-    }
-
-    if ($dateInput) {
-      var recordDate = sighting.datetime.trim();
-      var filterDate = $dateInput.value.trim(); 
-      if (recordDate === filterDate ) {
-         dateFound = true;
-      }
-    }
-
-    if ($shapeInput) {
-      var recordShape = sighting.shape.trim().toLowerCase();
-      var filterShape = $shapeInput.value.trim().toLowerCase(); 
-      if (recordShape === filterShape ) {
-         shapeFound = true;
-      }
-    }
-
-    if ($countryInput) {
-      var recordCountry = sighting.country.trim().toLowerCase();
-      var filterCountry = $countryInput.value.trim().toLowerCase(); 
-      if (recordCountry === filterCountry ) {
-        countryFound = true;
-      }
-    }
-
-    if ($cityInput) {
-      var recordCity = sighting.city.trim().toLowerCase();
-      var filterCity = $cityInput.value.trim().toLowerCase(); 
-      if (recordCity === filterCity ) {
-        cityFound = true;
-      }
-    }
+    // Looping through data set
+    for (let i = 0; i < filteredSightings.length; i++) {
     
-    if ( dateFound || cityFound || stateFound || countryFound || shapeFound ) {
-         return true
-    }
-    
-    // if ( dateFound && cityFound && stateFound && countryFound && shapeFound ){
-    //     return true
-    // } else { 
-    //     return false
-    // } 
+        // Insert a row
+        let $row = $tbody.insertRow(i);
 
-  });
-  // Now that the entire data set has been filtered and filteredSightings array
-  //   has been updated based on user input, render the new result set. 
-  renderTable();
-} // end of handleSearchButtonClick() function
-renderTable(); //This executed every time the page is reloaded.
+        // Get current object & keys
+        let currentSighting = filteredSightings[i];
+        let fields = Object.keys(currentSighting);
 
-    
- 
+        // Insert filteredSightings
+        for(let j = 0; j < fields.length; j++) {
+            let field = fields[j];
+            let $cell = $row.insertCell(j);
+            $cell.innerText = currentSighting[field];
+        };
+    };
+};
+
+// Event listener for submit button
+$submitButton.addEventListener("click", filterInput);
+
+// Function to filter date
+function filterDate(sighting) {
+    return sighting.datetime == $dateInput.value.trim().toLowerCase();
+};
+
+// Function to filter city
+function filterCity(sighting) {
+    return sighting.city == $cityInput.value.trim().toLowerCase();
+};
+
+// Function to filter state
+function filterState(sighting) {
+    return sighting.state == $stateInput.value.trim().toLowerCase();
+};
+
+// Function to filter country
+function filterCountry(sighting) {
+    return sighting.country == $countryInput.value.trim().toLowerCase();
+};
+
+// Function to filter shape
+function filterShape(sighting) {
+    return sighting.shape == $shapeInput.value.trim().toLowerCase();
+};
+
+// Function to filter input
+function filterInput(event) {
+
+    // Prevent default
+    event.preventDefault();
+
+    // Reseting data set each time button is clicked
+    filteredSightings = dataSet;
+
+    // Filters
+    if ($dateInput.value) {
+        filteredSightings = filteredSightings.filter(filterDate);
+    };
+
+    if ($cityInput.value) {
+        filteredSightings = filteredSightings.filter(filterCity);
+    };
+
+    if ($stateInput.value) {
+        filteredSightings = filteredSightings.filter(filterState);
+    };
+
+    if ($countryInput.value) {
+        filteredSightings = filteredSightings.filter(filterCountry);
+    };
+
+    if ($shapeInput.value) {
+        filteredSightings = filteredSightings.filter(filterShape);
+    };
+
+    if (!$dateInput && !$cityInput && !$stateInput && !$countryInput && !$shapeInput) {
+        filteredSightings = dataSet;
+    };
+
+    // Reset inputs
+    $dateInput.value = "";
+    $cityInput.value = "";
+    $stateInput.value = "";
+    $countryInput.value = "";
+    $shapeInput.value = "";
+
+    // Re-render table
+    $tbody.innerHTML = "";
+    renderTable();
+};
